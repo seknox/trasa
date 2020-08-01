85c97d02bbfcdb061b2b6f20129943052a4f08e7
bhrg3se <bhargab@seknox.com>
2020-08-01 10:30:20 +0545

    NFEAT: [build] docker files added

--- build/native/install.sh
+++ build/native/install.sh
@@ -1,7 +1,11 @@
-sudo apt-get -y update && \
+#!/usr/bin/env sh
 
-#for parsing json
-sudo apt-get -y install jq && \
+
+#Install postgres
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql
 
 #Install docker
 sudo apt-get -y remove docker docker-engine docker.io containerd runc && \
@@ -24,19 +28,6 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io && \
 
 sudo apt-get -y install ffmpeg && \
 
-
-
-
-
-
-INIT_CONFIG=`cat initconfig.json ` && \
-TRASA_SIGNUP_DATA=`echo $INIT_CONFIG | jq -r '.signup'` && \
-TRASA_HOSTNAME=`echo $INIT_CONFIG | jq -r '.hostname'` && \
-
-
-#echo INIT_CONFIG
-echo $TRASA_SIGNUP_DATA
-echo $TRASA_HOSTNAME
 
 sudo mkdir -p /etc/trasa && \
 sudo mkdir -p /etc/trasa/certs && \
@@ -68,19 +59,6 @@ sudo cp service-files-single-binary/* /etc/systemd/system && \
 sudo systemctl daemon-reload && \
 
 
-#sudo docker load -i bins/guacd.tar && \
-
-
-
-
-
-
-#mkdir certs my-safe-directory
-#cockroach cert create-ca --certs-dir=certs --ca-key=my-safe-directory/ca.key
-#cockroach cert create-node localhost beta.trasa.io $(hostname) --certs-dir=certs --ca-key=my-safe-directory/ca.key
-#
-
-
 
 #Start services
 sudo systemctl start cockroach && \
@@ -100,33 +78,8 @@ sudo systemctl start trasa
 sleep 10
 
 
-#signup and get orgID
-RESPONSE=`curl  --insecure 'http://localhost/idp/signup' -H 'content-type: application/json' --data "$TRASA_SIGNUP_DATA"` && \
-
-echo $RESPONSE
-
-STATUS=`echo $RESPONSE | jq '.status'`
-
-
-#if [ "$STATUS"  !=  "success"]
-#then
-#  echo "Failed to sign up"
-#  exit
-#endif
-
-ORG_ID=`echo $RESPONSE | jq '.data' | jq -r '.[0]'`
-
-
-echo $ORG_ID
-
-
-#write orgID in config
-sudo sed -i -e 's|<ORG_ID>|'$ORG_ID'|g' /etc/trasa/config/config.toml && \
-
 
 sudo systemctl restart trasa
-#'{"email":"bhargab@seknox.com","timezone":"Asia/Kathmandu","phoneNumber":9802084613,"orgName":"Seknox_Staging","userName":"bhrg3se","country":"Nepal"}'
-
 
 
 echo ""
