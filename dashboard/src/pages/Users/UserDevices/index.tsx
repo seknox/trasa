0e94d79828f63debcebb0f90829d6f2e3072bdbf
sakshyam shah <sakshyam.shah@gmail.com>
2020-08-02 17:28:27 +0545

    fix: [dashboard] added renderFor props in device ui render. added log table in service access-stats page

--- dashboard/src/pages/Users/UserDevices/index.tsx
+++ dashboard/src/pages/Users/UserDevices/index.tsx
@@ -149,21 +149,6 @@ export default function Device(props: any) {
         />
       </Grid>
 
-      {/* <Grid item xs={12}>
-        <Typography variant="h3">Browsers</Typography>
-        <br />
-        <DeviceList
-          userDevices={userDevices.browser}
-          deviceType="browser"
-          deleteUserDlgOpen={openDeviceDeleteDlg}
-          open={deviceDeleteState}
-          close={closeDeviceDeleteDlg}
-          deleteFunc={removeUserDevice}
-          deleteDevice={setDeleteDeviceFunc}
-          showDeviceDetail={showDeviceDetail}
-        />
-      </Grid> */}
-
       <Grid item xs={12}>
         <Typography variant="h3">Workstations</Typography>
         {/* <Divider light /> */}
@@ -177,6 +162,7 @@ export default function Device(props: any) {
           deleteFunc={removeUserDevice}
           deleteDevice={setDeleteDeviceFunc}
           showDeviceDetail={showDeviceDetail}
+          renderFor={props.renderFor}
         />
       </Grid>
     </Grid>
