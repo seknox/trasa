2b1c9cff8090de4adc2bdb619132d432d655bcf6
sakshyam shah <sakshyam.shah@gmail.com>
2020-08-06 23:38:44 +0545

    fix: [tests] using is-reachable till https:app.trasa is available

--- tests/__tests__/suite.js
+++ tests/__tests__/suite.js
@@ -1,10 +1,20 @@
 require('expect-puppeteer')
+const isReachable = require('is-reachable');
 
 import { logintests } from '../src/login'
-import {sshtest} from "../src/ssh";
+// import {sshtest} from "../src/ssh";
 
 jest.setTimeout(30000)
 beforeAll(async () => {
+
+
+  let up = false 
+  while (up !== true) {
+  await isReachable('app.trasa:443').then(u => up = u)
+  }
+
+
+  
     //const page = await browser.newPage()
     await page.goto('https://app.trasa')
   })
--- tests/pretest.sh
+++ tests/pretest.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env sh
 
-cd ../build/test && \
-docker-compose build && \
-docker-compose up --force-recreate & \
-../build/test/wait-for-it.sh 127.0.0.1:443 -- echo "server is up"
-jest
+cd ../build/test && docker-compose build && docker-compose up &
+
+# BACK_PID=$!
+# wait $BACK_PID
+
+#../build/test/wait-for-it.sh 127.0.0.1:443 -- echo "=========================== SERVER IS UP ===================================="
+# echo "=========================== SERVER IS UP ===================================="
+jest 
+
 cd ../build/test && docker-compose down
