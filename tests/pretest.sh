900b45c790b535e4ea56eb60f76fdc5c83bd7360
bhrg3se <bhargab@seknox.com>
2020-08-06 23:27:28 +0545

    fix: [test] wait for server start fixed

--- tests/pretest.sh
+++ tests/pretest.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env sh
 
-cd ../build/test && \
-docker-compose build && \
-docker-compose up --force-recreate & \
-../build/test/wait-for-it.sh 127.0.0.1:443 -- echo "server is up"
+cd ../build/test && docker-compose build && docker-compose up --force-recreate -d && cd ../../tests
+
+
+max_iterations=30
+wait_seconds=1
+http_endpoint="https://app.trasa/idp/login"
+
+iterations=0
+while true
+do
+	((iterations++))
+	echo "Attempt $iterations"
+	sleep $wait_seconds
+
+	http_code=$(curl  --insecure -XPOST -s -o /tmp/result.txt -w '%{http_code}' "$http_endpoint";)
+
+	if [ "$http_code" -eq 200 ]; then
+		echo "Server Up"
+		break
+	fi
+
+	if [ "$iterations" -ge "$max_iterations" ]; then
+		echo "Loop Timeout"
+		exit 1
+	fi
+done
+
 jest
 cd ../build/test && docker-compose down
--- tests/src/login.js
+++ tests/src/login.js
@@ -33,26 +33,28 @@ export const logintests = () => {
     await page.click('#submit')
     // await page.screenshot({path: 'shot.png'});
 
-    let loginResp = await page.waitForResponse('https://app.trasa/idp/login');
+    let loginResp = await page.waitForResponse(TRASA_HOSTNAME+'/idp/login');
 
     expect(loginResp.status()).toBe(200)
 
       await expect(page).toMatch('Get TRASA app for your mobile device.')
 
+
       const deviceData = await loginResp.json()
-      TOTP_SSC=deviceData.totpSSC
+      TOTP_SSC=deviceData.data[0].totpSSC
 
       await axios({
           method: 'post',
-          url: deviceData.cloudProxyURL + '/api/v1/passmydevicedetail',
+          url: deviceData.data[0].cloudProxyURL + '/api/v1/passmydevicedetail',
           data: {
-              deviceId: deviceData.deviceID,
+              deviceId: deviceData.data[0].deviceID,
               fcmToken: 'askdnsanduasdosuajasdguyagsdygsac7sacsyaubchjasb',
               publicKey: '',
               deviceFinger: JSON.stringify({"deviceInfo": {"brand": "samsung", "deviceModel": "SM-A305F", "deviceName": "Galaxy A30", "deviceVersion": "", "machineID": "81836ac40d412942", "manufacturer": "samsung"}, "deviceOS": {"autoUpdate": true, "debugModeEnabled": false, "isEmulator": false, "jailBroken": false, "kernelType": "", "kernelVersion": "", "latestSecurityPatch": "2020-06-01", "osName": "Android", "osVersion": "10", "pendingUpdates": null, "readableVersion": ""}, "endpointSecurity": {"deviceEncryptionEnabled": false, "deviceEncryptionMeta": "", "epsConfigured": false, "epsMeta": "", "epsVendorName": "", "epsVersion": "", "firewallEnabled": false, "firewallPolicy": ""}, "lastCheckedTime": 0, "loginSecurity": {"autologinEnabled": false, "idleDeviceScreenLock": false, "idleDeviceScreenLockTime": "", "loginMethod": "", "passwordLastUpdated": "", "remoteLoginEnabled": false, "tfaConfigured": false}, "networkInfo": {"domainControl": false, "domainName": "", "hostname": "SWDI7613", "interfaceName": "wifi", "ipAddress": "192.168.100.125", "macAddress": "FC:AA:B6:57:71:8B", "networkName": "", "networkSecurity": "", "openWifiConn": false, "wirelessNetwork": true}}),
           },
+      }).then(r=>{
+          expect(r.data.status).toBe("success")
       })
-
       await page.goto("https://app.trasa/login",{waitUntil:"load"})
 
       await page.type('#email', loginData.email)
@@ -74,7 +76,7 @@ export const logintests = () => {
   await page.type("[name=totpVal]",getTotp(TOTP_SSC))
   await page.keyboard.press("Enter")
 
-  let tfaResp = await page.waitForResponse('https://app.trasa/idp/login/tfa');
+  let tfaResp = await page.waitForResponse(TRASA_HOSTNAME+'/idp/login/tfa');
 
 
   expect(tfaResp.status()).toBe(200)
@@ -87,7 +89,7 @@ export const logintests = () => {
   // })
 
 
-  await page.screenshot({path: 'screen.png'});
+  //await page.screenshot({path: 'screen.png'});
 
 
 
