0df9866f588927a69cdbe16d726e7d907e704517
bhrg3se <bhargab@seknox.com>
2020-08-06 14:36:49 +0545

    wip: [test] added ssh test and mock data

--- tests/__tests__/suite.js
+++ tests/__tests__/suite.js
@@ -1,6 +1,8 @@
 require('expect-puppeteer')
 
 import { logintests } from '../src/login'
+import {sshtest} from "../src/ssh";
 
 
-describe('Login', logintests)
+// describe('Login', logintests)
+describe("SSH",sshtest)
