209dc4ea406842a983726ed4bc7d041ffc242cf8
bhrg3se <bhargab@seknox.com>
2020-09-04 18:09:10 +0545

    fix :[test] "download private key" test moved to ssh test

--- tests/server/my_test.go
+++ tests/server/my_test.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-chi/chi"
-	"github.com/seknox/ssh"
-	"github.com/seknox/trasa/server/accessproxy/sshproxy"
 	"github.com/seknox/trasa/server/api/my"
 	"github.com/seknox/trasa/server/models"
 	"github.com/seknox/trasa/tests/server/testutils"
@@ -171,42 +169,6 @@ func TestAuthMeta(t *testing.T) {
 
 	if data.IsPasswordRequired {
 		t.Errorf(`GetAuthMeta.IsPasswordRequired  want: %v got: %v`, false, true)
-	}
-
-}
-
-func TestDownloadKey(t *testing.T) {
-	req, err := http.NewRequest("GET", "", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(testutils.AddTestUserContext(my.GenerateKeyPair))
-
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-	testutils.MockPrivateKey2 = string(rr.Body.Bytes())
-
-	k, err := ssh.ParsePrivateKey(rr.Body.Bytes())
-	if err != nil {
-		t.Errorf(`invalid user key`)
-	}
-
-	user, err := sshproxy.SSHStore.GetUserFromPublicKey(k.PublicKey(), testutils.MockOrgID)
-	if err != nil {
-		t.Errorf(`incorrect user key`)
-	}
-
-	if user.ID != testutils.MockUserID {
-		t.Errorf(`incorrect user ID, want=%v got=%v`, testutils.MockUserID, user.ID)
 	}
 
 }
