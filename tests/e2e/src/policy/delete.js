519e1bc1246c893a4252ce9fce19e6e6a6eca8e1
sshahcodes <sakshyam.shah@gmail.com>
2021-01-06 12:25:16 +0545

    ref:[dashboard] changed dashboard route name 'Control' to 'Policies'

--- tests/e2e/src/policy/delete.js
+++ tests/e2e/src/policy/delete.js
@@ -1,58 +1,46 @@
 //MUIDataTableSelectCell-0
 
-import Constants from '../../Constants'
+import Constants from "../../Constants";
 
-require('expect-puppeteer')
-const policyData=require('../../mock_data/policies')
-
-
+require("expect-puppeteer");
+const policyData = require("../../mock_data/policies");
 
 //create user
 export const DeletePolicy = () => {
-    beforeAll(async () => {
-        await page.goto(Constants.TRASA_DASHBOARD+'/control',{waitUntil:'load'})
-    })
+  beforeAll(async () => {
+    await page.goto(Constants.TRASA_DASHBOARD + "/policies", {
+      waitUntil: "load",
+    });
+  });
 
-    it('Should update a policy '+Constants.TRASA_DASHBOARD+'/control', async () => {
-        const testPolicy=policyData.policies[0]
-        // await page.goto(Constants.TRASA_DASHBOARD+"/users")
+  it(
+    "Should update a policy " + Constants.TRASA_DASHBOARD + "/policies",
+    async () => {
+      const testPolicy = policyData.policies[0];
+      // await page.goto(Constants.TRASA_DASHBOARD+"/users")
 
+      await expect(page).toMatch("Policies");
 
-        await expect(page).toMatch('Policies')
+      await page.waitForSelector('[id="new policy"]', { timeout: 5000 });
 
-        await page.waitForSelector('[id="new policy"]',{timeout:5000})
+      await page.click("#MUIDataTableSelectCell-0");
 
+      const respPromise = page.waitForResponse((r) =>
+        r.url().includes("policy/delete")
+      );
 
+      await page.click("button[title=Delete]");
 
-        await page.click('#MUIDataTableSelectCell-0')
+      let resp = await respPromise;
 
-        const respPromise=page.waitForResponse(r=>r.url().includes('policy/delete'))
+      await expect(resp.status()).toBe(200);
 
-        await page.click('button[title=Delete]')
+      // const respData=await resp.json()
+      //
+      // await expect(respData.status).toBe('success')
 
-
-
-        let resp = await respPromise
-
-        await expect(resp.status()).toBe(200)
-
-        // const respData=await resp.json()
-        //
-        // await expect(respData.status).toBe('success')
-
-
-
-
-        // await page.waitFor(20000)
-        //  await page.waitFor(10000)
-
-
-    })
-
-
-
-
-}
-
-
-
+      // await page.waitFor(20000)
+      //  await page.waitFor(10000)
+    }
+  );
+};
